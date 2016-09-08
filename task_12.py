8f3a43a9df67616b6283b75750bb5a84d593abfa
Laura Pomeroy <laura.pomeroy@spsmail.cuny.edu>
2016-09-08 03:25:34 +0000

    Decimals

---
+++ task_12.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""A little docstring"""
+
+import decimal
+
+INVAL = 1
+FLOATVAL = decimal.Decimal('0.1')
+DECVAL = decimal.Decimal('0.1')
+FRACVAL = "1/10"
